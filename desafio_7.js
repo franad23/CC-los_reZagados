5260fcce68c735e2da71931ec28b9a44627fb05a
MariaLauraElias <eliasmarialaura@gmail.com>
2024-08-03 18:11:27 -0300

    desafío 7 completado

--- desafio_7.js
+++ desafio_7.js
@@ -3,6 +3,19 @@ const n = 4;
 
 
 function juegoDePalabras (arr,n){
+
+  if(!(arr instanceof Array)) return console.log('El valor ingresado no es un array');
+  if(arr.length === 0) return console.log('El array esta vacio');
+  
+  for (let i of arr){
+    if (typeof i !== 'string') return console.log(`El valor ${i} no es un string`);
+  }
+  if(!n) return console.log('No se ingreso un numero');
+  if(typeof n !== 'number') return console.log('El valor ingresado no es un nmero');
+  if(n <= 0) return console.log('El número debe ser mayor a 0');
+
+
+
   let resultado = arr.filter((palabra) => {
     return palabra.length === n
     }
