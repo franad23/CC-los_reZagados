9a2f335a627656da713843e73e9b9bbb0c9f603c
priscilabgmoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2024-08-03 16:30:38 -0300

    se agrega desafio 3

--- desafio_3.js
+++ desafio_3.js
@@ -1,4 +1,4 @@
-const letras = ['a','b','c','d','e','f','g','h','i','j','k','l', 'ñ','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 function cifradoCesar  (texto, dezplazamiento){
     let textoCifrado = ""; 
